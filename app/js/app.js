f30caa69f9679fb98140f24ef69acc57cbfd7285
Steffen Kühne <stekhn@gmail.com>
2014-10-03 23:57:02 +0200

    Add directive for page switching

--- app/js/app.js
+++ app/js/app.js
@@ -12,11 +12,36 @@
 
 	}]);
 
+	app.directive("content", function() {
+		return {
+			restrict: "E",
+			templateUrl: "templates/content.html",
+			controller: function() {
+				this.tab = 1;
+				this.isSet = function(checkTab) {
+					return this.tab === checkTab;
+				};
+				this.setTab = function(activeTab) {
+					this.tab = activeTab;
+				};
+			},
+			controllerAs: "tab"
+		};
+	});
+
 	app.directive('project', function () {
 
 		return {
 			restrict: 'E',
 			templateUrl: 'templates/project.html'
+		};
+	});
+
+	app.directive('projectList', function () {
+
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/project-list.html'
 		};
 	});
 
