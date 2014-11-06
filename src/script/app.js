fa40e5ec1701f21324a3d59c0e0d04d6f1f63182
Steffen Kühne <stekhn@gmail.com>
2014-11-06 23:32:30 +0100

    Update grunt build script

--- src/script/app.js
+++ src/script/app.js
@@ -5,6 +5,7 @@
 	app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
 		$locationProvider.hashPrefix('!');
+
 		$routeProvider
 			.when("/", {templateUrl: "template/project-list.html"})
 			.when("/project/:name", {templateUrl: "template/project.html", controller: "ProjectCtrl"})
@@ -16,7 +17,7 @@
 			.otherwise("/", {templateUrl: "template/project-list.html"});
 	}]);
 
-	app.run(function ($rootScope, $location, $route) {
+	app.run(['$rootScope', '$location', '$route', function ($rootScope, $location, $route) {
 
 		$rootScope.config = {};
 		$rootScope.config.app_url = $location.url();
@@ -38,7 +39,7 @@
 
 			$rootScope.layout.loading = false;
 		});
-	});
+	}]);
 
 	app.controller('JsonLoaderCtrl', ['$scope', '$http', function ($scope, $http) {
 
@@ -49,14 +50,14 @@
 
 	}]);
 
-	app.controller('NavCtrl', function($scope, $location) {
+	app.controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
 
 		$scope.isActive = function(route) {
 			return route === $location.path();
 		};
-	});
+	}]);
 
-	app.controller('ProjectCtrl', function ($scope, $routeParams) {
+	app.controller('ProjectCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
 
 		var current;
 
@@ -70,99 +71,99 @@
 		}
 		
 		$scope.project = $scope.projects[current];
-	});
-
-	angular.module('toDashCase', []).filter('dashcase', function() {
-		return function(input) {
-			return input.replace(/\s+/g, '-').toLowerCase();
-		};
-	});
-
-	angular.module('feedReader', []).controller('RssFeedCtrl', ['$http', '$interval', '$scope', '$sce', function ($http, $interval, $scope, $sce) {
-
-		$scope.articles = [ ];
-		$scope.rssFeed = 'http://datenkritik.de/feed/';
-
-		$scope.existingArticles = function () {
-
-			return _.find($scope.articles, function (a) {
-				return !a.cleared;
-			}) !== null;
-		};
-	
-		$scope.showOrHideAll = function () {
-
-			var markAsHide = _.every($scope.articles, function (a) {
-				return a.show;
-			});
-
-			_.each($scope.articles, function (el, index, list) {
-				el.show = !markAsHide;
-			});
-		};
-	
-		var hostname = (function () {
-
-			var a = document.createElement('a');
-
-			return function (url) {
-				a.href = url;
-				return a.hostname;
-			};
-		})();
-	
-		var parseEntry = function (el) {
-
-			$scope.html = el.content;
-			$scope.trustedHtml = $sce.trustAsHtml($scope.html);
-
-			return {
-				title: el.title,
-				content: $scope.trustedHtml,
-				read: false,
-				date: el.publishedDate || el.pubDate,
-				link: el.link,
-				shortLink : hostname(el.link)
-			};
-		};
-	
-		var parseRSS = function (url) {
-
-			return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
-		};
-	
-		$scope.updateModel = function () {
-
-			parseRSS($scope.rssFeed).then(function (data) {
-
-				if (data === null) {
-					return;
-				}
-	
-				var mostRecentDate = null;
-
-				var entries = _.map(data.data.responseData.feed.entries, function (el) {
-					return parseEntry(el);
-				});
-	
-				if (mostRecentDate !== null) {
-
-					entries = _.filter(entries, function (el) {
-						return el.date < mostRecentDate;
-					});
-				}
-	
-				$scope.articles = _.union($scope.articles, entries);
-
-				$scope.articles = _.sortBy($scope.articles, function (el) {
-					return el.date;
-				});
-			});
-		};
-
-		// update initially
-		$scope.updateModel();
-
 	}]);
 
 }());
+
+angular.module('toDashCase', []).filter('dashcase', function() {
+	return function(input) {
+		return input.replace(/\s+/g, '-').toLowerCase();
+	};
+});
+
+angular.module('feedReader', []).controller('RssFeedCtrl', ['$http', '$interval', '$scope', '$sce', function ($http, $interval, $scope, $sce) {
+
+	$scope.articles = [ ];
+	$scope.rssFeed = 'http://datenkritik.de/feed/';
+
+	$scope.existingArticles = function () {
+
+		return _.find($scope.articles, function (a) {
+			return !a.cleared;
+		}) !== null;
+	};
+
+	$scope.showOrHideAll = function () {
+
+		var markAsHide = _.every($scope.articles, function (a) {
+			return a.show;
+		});
+
+		_.each($scope.articles, function (el, index, list) {
+			el.show = !markAsHide;
+		});
+	};
+
+	var hostname = (function () {
+
+		var a = document.createElement('a');
+
+		return function (url) {
+			a.href = url;
+			return a.hostname;
+		};
+	})();
+
+	var parseEntry = function (el) {
+
+		$scope.html = el.content;
+		$scope.trustedHtml = $sce.trustAsHtml($scope.html);
+
+		return {
+			title: el.title,
+			content: $scope.trustedHtml,
+			read: false,
+			date: el.publishedDate || el.pubDate,
+			link: el.link,
+			shortLink : hostname(el.link)
+		};
+	};
+
+	var parseRSS = function (url) {
+
+		return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+	};
+
+	$scope.updateModel = function () {
+
+		parseRSS($scope.rssFeed).then(function (data) {
+
+			if (data === null) {
+				return;
+			}
+
+			var mostRecentDate = null;
+
+			var entries = _.map(data.data.responseData.feed.entries, function (el) {
+				return parseEntry(el);
+			});
+
+			if (mostRecentDate !== null) {
+
+				entries = _.filter(entries, function (el) {
+					return el.date < mostRecentDate;
+				});
+			}
+
+			$scope.articles = _.union($scope.articles, entries);
+
+			$scope.articles = _.sortBy($scope.articles, function (el) {
+				return el.date;
+			});
+		});
+	};
+
+	// update initially
+	$scope.updateModel();
+
+}]);
