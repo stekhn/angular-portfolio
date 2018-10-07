a3f1459bbfd7edaad9eefc4c64306a85c85b2f3d
Steffen Kühne <stekhn@gmail.com>
2018-10-07 20:59:08 +0200

    Fix bug with German umlauts in dashcase filter

--- src/script/components/dashcaseFilter.js
+++ src/script/components/dashcaseFilter.js
@@ -2,14 +2,12 @@ app.filter('dashcase', function () {
 
   return function (input) {
 
-    input = input.replace(/\s+/g, '-')
-      .toLowerCase()
-      .replace('ä', 'ae')
-      .replace('ö', 'oe')
-      .replace('ü', 'ue')
-      .replace('ß', 'ss')
+    return input.toLowerCase()
+      .replace(/\s+/g, '-')
+      .replace(/ä/g, 'ae')
+      .replace(/ö/g, 'oe')
+      .replace(/ü/g, 'ue')
+      .replace(/\u00df/g, 'ss')
       .replace(/[^a-z0-9-]/g, '');
-
-    return input;
   };
 });
