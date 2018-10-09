4766f4e5e82d696218bdabee1d3602fd7279a5ee
Steffen Kühne <stekhn@gmail.com>
2018-10-09 22:40:19 +0200

    Update JSONP request

--- src/script/components/feedReader.js
+++ src/script/components/feedReader.js
@@ -41,13 +41,15 @@ angular.module('feedReader', [])
 
       var parseRSS = function (url) {
 
-        return $http.jsonp(
-          'https://query.yahooapis.com/v1/public/' +
-          'yql?q=select%20*%20from%20xml%20where%20url%20%3D%20"' +
-          encodeURIComponent(url) +
-          '"&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys' +
-          '&callback=JSON_CALLBACK'
-        );
+        var queryUrl = 'https://query.yahooapis.com/v1/public/yql';
+
+        var params = {
+          'q': 'select * from xml where url = "' + url + '"',
+          'format': 'json',
+          'env': 'store://datatables.org/alltableswithkeys'
+        };
+
+        return $http.jsonp($sce.trustAsResourceUrl(queryUrl), { params: params });
       };
 
       $scope.updateModel = function () {
